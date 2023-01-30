6fb285ae4d06cbc360af8c16d3b904482f946687
csnow0 <123226292+csnow0@users.noreply.github.com>
2023-01-30 17:56:00 -0600

    Create derp.py

---
+++ derp.py
@@ -0,0 +1,9 @@
+import apple, banana, grapes, kiwi, strawberry
+from parent.child import annoy
+import stupid.person
+import mark_zuckerberg as lizard_person
+
+import numpy as np
+from scipy.stats import burr
+import matpotlib.pyplot as plt
+import os, sys, re, string
